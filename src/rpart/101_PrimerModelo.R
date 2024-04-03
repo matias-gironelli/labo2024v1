384bd5a0f6d075cc31f6634fe50203dee665eede
matias-gironelli <matias.e.gironelli@gmail.com>
2024-04-03 21:28:21 +0000

    auto commit 1       20240403 212820

--- src/rpart/101_PrimerModelo.R
+++ src/rpart/101_PrimerModelo.R
@@ -21,10 +21,10 @@ modelo <- rpart(
         formula = "clase_ternaria ~ .",
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
-        cp = -0.3, # esto significa no limitar la complejidad de los splits
-        minsplit = 0, # minima cantidad de registros para que se haga el split
-        minbucket = 1, # tamaño minimo de una hoja
-        maxdepth = 3
+        cp = -0.1, # esto significa no limitar la complejidad de los splits
+        minsplit = 400, # minima cantidad de registros para que se haga el split
+        minbucket = 133.3333333, # tamaño minimo de una hoja
+        maxdepth = 6
 ) # profundidad maxima del arbol
 
 
